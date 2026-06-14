c26e2f8373f2d1bace85c3a52172a517591b0133
Your Name <your@email.com>
2026-06-14 07:14:44 +0000

    Added custom RQF docttypes

---
+++ saadaa/saadaa/doctype/request_for_quotations/request_for_quotations.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ram and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Request for Quotations", {
+// 	refresh(frm) {
+
+// 	},
+// });
