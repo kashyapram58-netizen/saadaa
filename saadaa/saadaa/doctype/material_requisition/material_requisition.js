24c99333399e28147920988a242caf89a9476896
Your Name <your@email.com>
2026-06-15 04:03:40 +0000

    Added link between MR and RFQ

--- saadaa/saadaa/doctype/material_requisition/material_requisition.js
+++ saadaa/saadaa/doctype/material_requisition/material_requisition.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2026, Ram and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Material Requisition", {
-// 	refresh(frm) {
+frappe.ui.form.on('Material Requisition', {
+    refresh: function (frm) {
+        if (!frm.is_new() && !frm.is_dirty()) {
 
-// 	},
-// });
+            // Add Create menu
+            frm.add_custom_button(__('Request for Quotations'), function () {
+                // frappe.new_doc opens a blank form with pre-filled fields
+                frappe.new_doc('Request For Quotations', {
+                    'material_requisition': frm.doc.name
+                });
+            }, __('Create'));
+
+            frm.add_custom_button(__('Purchase Order'), function () {
+                frappe.new_doc('Purchase Orders', {
+                    'material_requisition': frm.doc.name
+                });
+            }, __('Create'));
+        }
+    }
+});
--- saadaa/saadaa/doctype/request_for_quotations/request_for_quotations.js
+++ saadaa/saadaa/doctype/request_for_quotations/request_for_quotations.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2026, Ram and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Request for Quotations", {
+// frappe.ui.form.on("Request For Quotations", {
 // 	refresh(frm) {
 
 // 	},
