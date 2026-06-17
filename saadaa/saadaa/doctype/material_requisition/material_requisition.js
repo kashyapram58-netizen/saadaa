1a4b72737dc9e9660b39dd8f6bd05cfc87db5edb
Your Name <your@email.com>
2026-06-17 11:39:41 +0000

    Added workflow changes to MR and Vq

--- saadaa/saadaa/doctype/material_requisition/material_requisition.js
+++ saadaa/saadaa/doctype/material_requisition/material_requisition.js
@@ -4,10 +4,8 @@
 frappe.ui.form.on('Material Requisition', {
     refresh: function (frm) {
         if (!frm.is_new() && !frm.is_dirty()) {
-
             // Add Create menu
             frm.add_custom_button(__('Request for Quotations'), function () {
-                // frappe.new_doc opens a blank form with pre-filled fields
                 frappe.new_doc('Request For Quotations', {
                     'material_requisition': frm.doc.name
                 });
@@ -18,6 +16,34 @@ frappe.ui.form.on('Material Requisition', {
                     'material_requisition': frm.doc.name
                 });
             }, __('Create'));
+        }
+    },
+
+    // --- NEW: Date Automation Requirements ---
+    onload: function (frm) {
+        // Auto-set Transaction Date on new forms
+        if (frm.is_new()) {
+            frm.set_value('transaction_date', frappe.datetime.get_today());
+        }
+    },
+
+    required_by: function (frm) {
+        // Auto-populate 'required_by_date' in the items table when header changes
+        if (frm.doc.required_by) {
+            frm.doc.items.forEach(function (row) {
+                frappe.model.set_value(row.doctype, row.name, 'required_by_date', frm.doc.required_by);
+            });
+            frm.refresh_field('items');
+        }
+    }
+});
+
+// Auto-populate when adding a new row to the table
+frappe.ui.form.on('Material Requisition Item', {
+    form_render: function (frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (frm.doc.required_by && !row.required_by_date) {
+            frappe.model.set_value(cdt, cdn, 'required_by_date', frm.doc.required_by);
         }
     }
 });
