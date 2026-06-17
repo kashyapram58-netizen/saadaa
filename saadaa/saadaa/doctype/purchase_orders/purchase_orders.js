5f890ed4c4a8d7e48196a8701b417263533b4374
Your Name <your@email.com>
2026-06-17 09:05:19 +0000

    Linked VQs To PO and performed mapping

--- saadaa/saadaa/doctype/purchase_orders/purchase_orders.js
+++ saadaa/saadaa/doctype/purchase_orders/purchase_orders.js
@@ -1,4 +1,14 @@
-frappe.ui.form.on('Purchase Orders', {
-	refresh: function(frm) {
+frappe.listview_settings['Purchase Orders'] = {
+	onload: function (listview) {
+		listview.page.add_field({
+			fieldname: "status",
+			fieldtype: "Select",
+			label: __("Status"),
+			options: "\nDraft\nPending\nAudited\nSubmitted\nApproved\nRejected\nCancelled",
+			onchange: function () {
+				listview.filter_area.add([["Purchase Orders", "status", "=", this.value]]);
+				listview.run();
+			}
+		});
 	}
-});
+};
--- saadaa/saadaa/doctype/purchase_orders/purchase_orders.py
+++ saadaa/saadaa/doctype/purchase_orders/purchase_orders.py
@@ -3,3 +3,10 @@ from frappe.model.document import Document
 
 class PurchaseOrders(Document):
 	pass
+def get_dashboard_info(doc):
+    return {
+        "fieldname": "purchase_orders",
+        "transactions": [
+            {"label": "Reference", "items": ["Vendor Quotation"]}
+        ]
+    }
